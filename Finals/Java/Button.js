c571071fef5b85b32471bca4aa9505e8cdbaf43a
JessieMendoza_HUMCOM1L_20214135 <97872976+JessieMendoza-HUMCOM1L-20214135@users.noreply.github.com>
2022-05-14 19:41:56 +0800

    Add files via upload

---
+++ Finals/Java/Button.js
@@ -0,0 +1,3 @@
+function myFunction() {
+    alert("Phone Number: (613) 889-1858, (588) 919-6863, (823) 728-6882");
+  }
---
+++ Finals/Java/Buy Now.js
@@ -0,0 +1,12 @@
+function Buy1() {
+    alert("Congrats you bought a Oculus VR");
+  }
+  function Buy2() {
+    alert("Congrats you bought a Samsung VR");
+  }
+  function Buy3() {
+    alert("Congrats you bought a HTC VR");
+  }
+  function Buy4() {
+    alert("Congrats you bought a Sony VR");
+  }
